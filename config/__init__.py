ef8d1485dde06c94fa6b95b454284098dcae7842
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2023-04-05 23:01:30 +0530

    update

--- config/__init__.py
+++
@@ -1,2 +0,0 @@
-from .diffconfig import *
-from .dataconfig import *
--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from typing import Union
-from .unet import BeatGANsUNetModel, BeatGANsUNetConfig
-from .unet_autoenc import BeatGANsAutoencConfig, BeatGANsAutoencModel
-
-Model = Union[BeatGANsUNetModel, BeatGANsAutoencModel]
-ModelConfig = Union[BeatGANsUNetConfig, BeatGANsAutoencConfig]
